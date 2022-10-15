bfbd3985b83f1de2b1b35f404f9caecf6ffbe72b
Aala Eddine Bousskoul <70477189+aalaebl@users.noreply.github.com>
2022-10-15 22:02:05 +0100

    test PWA again

--- src/next.config.js
+++ src/next.config.js
@@ -1,27 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+// const withPWA = require("next-pwa");
 
-module.exports = nextConfig;
+// const nextConfig = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     disable: process.env.NODE_ENV === "development",
+//     skipWaiting: true,
+//   },
+// });
 
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+// module.exports = nextConfig;
 
-if (!(process.env.NODE_ENV === "development")) {
-  nextConfigWithPWA = withPWA({
-    pwa: {
-      dest: "public",
-      register: true,
-      skipWaiting: true,
-      runtimeCaching,
-      buildExcludes: [/manifest.json$/],
-      maximumFileSizeToCacheInBytes: 5000000,
-    },
-    ...nextConfig,
-  });
-}
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  // next.js config
+});
+
+// const runtimeCaching = require("next-pwa/cache");
+
+// if (!(process.env.NODE_ENV === "development")) {
+//   nextConfigWithPWA = withPWA({
+//     pwa: {
+//       dest: "public",
+//       register: true,
+//       skipWaiting: true,
+//       runtimeCaching,
+//       buildExcludes: [/manifest.json$/],
+//       maximumFileSizeToCacheInBytes: 5000000,
+//     },
+//     ...nextConfig,
+//   });
+// }
 
 // module.exports = withPWA({
 //   pwa: {
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,14 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-256x256.png"></link>
         <meta name="theme-color" content="#fff" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Rhymes" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/favicon.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
       <body>
         <Main />
