03841ca6dd09211f653bf8170d141f4f59b18999
aalaebl <aalaeedine2000@outlook.fr>
2023-02-15 11:19:35 +0100

    new sw

--- src/next.config.js
+++ src/next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+  fallbacks: {
+    // image: "/static/images/fallback.png",
+    document: "/offline.html", // if you want to fallback to a custom    page other than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+});
+
+module.exports = withPWA({
   i18n: {
     locales: ["de", "en", "es", "fr", "hi", "it", "nl", "ru"],
     defaultLocale: "en",
   },
-};
+});
